90b5f0720774413c654e6ce4c00578148d11b3e8
Bohdan Babchuk <babchuk.bohdan@gmail.com>
2021-05-02 11:11:53 +0300

    Add dotenv module

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+require('dotenv').config()
+
 const userRouter = require('./routes/userRoutes')
 const requestsRouter = require('./routes/requestsRoutes')
 const driverRouter = require('./routes/driverRoutes')
 
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(express.json())
