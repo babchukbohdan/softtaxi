ee24d39c510795abd61d5b7a73a3c18bcd47584c
Bohdan Babchuk <babchuk.bohdan@gmail.com>
2021-05-09 00:42:48 +0300

    Add login method to login-form comp. Bind car type to icon and select elem.

--- frontend/src/app/icon/icon.component.ts
+++ frontend/src/app/icon/icon.component.ts
@@ -9,6 +9,7 @@ export class IconComponent implements OnInit {
   @Input() name: string;
   @Input() size: string;
   @Input() className: string;
+  @Input() isActive: boolean;
 
   constructor() {}
 
--- frontend/src/app/login-form/login-form.component.ts
+++ frontend/src/app/login-form/login-form.component.ts
@@ -1,15 +1,47 @@
+import { FormsModule } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
+  phone: 11;
+  password: 'test';
+  constructor() {}
 
-  constructor() { }
+  login() {
+    const data = {
+      phone: this.phone,
+      password: this.password,
+    };
+    const res = fetch('http://localhost:8080/user/login', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
 
-  ngOnInit(): void {
+    console.log(data, 'data for login');
+
+    res
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.token) {
+          localStorage.setItem('token', res.token);
+        }
+      });
   }
 
+  ngOnInit(): void {}
 }
